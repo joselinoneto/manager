09567d4b25eae9e8fba0491fac561f8256db708c
José Neto <jneto@blissapplications.com>
2022-09-11 18:06:56 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# manager
+
+A description of this package.
---
+++ Tests/managerTests/managerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import manager
+
+final class managerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(manager().text, "Hello, World!")
+    }
+}
