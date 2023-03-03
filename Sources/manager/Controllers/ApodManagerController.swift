04782194e1226c6dbebd1f651611b31ded652799
José Neto <jneto@blissapplications.com>
2023-03-03 10:50:20 +0000

    async save

--- Sources/manager/Controllers/ApodManagerController.swift
+++ Sources/manager/Controllers/ApodManagerController.swift
@@ -57,10 +57,10 @@ public class ApodManagerController {
     
     private func saveItems(_ items: [NasaApodDto]?) async throws {
         let itemsAdd: [ApodStorage] = items?.map { ApodStorage($0) } ?? []
-        try storageController.saveItemsSql(itemsAdd)
-        self.items = try getAll()?.mapToEntity()
-        // for item in itemsAdd {
-        //    try await storageController.asyncSaveItem(item)
-        // }
+        // try storageController.saveItemsSql(itemsAdd)
+        // self.items = try getAll()?.mapToEntity()
+        for item in itemsAdd {
+            try await storageController.asyncSaveItem(item)
+        }
     }
 }
